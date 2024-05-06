aa1df33e4c0e00af3dbd04563cba535f1507841d
Katiasun <katjatolok3@gmail.com>
2024-05-06 23:22:59 +0200

    create component Header and write code

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">Contexto</h1>
+      <p className="header-description">
+        Guess the word based on the clues provided by your friends!
+      </p>
+    </header>
+  );
+}
