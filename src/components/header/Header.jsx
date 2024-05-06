17d024c7d1d2cb30d009b7b04fcba4f828bf2851
Katiasun <katjatolok3@gmail.com>
2024-05-06 23:58:43 +0200

    connected the styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
-import "./header.module.css";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1 className="header-title">Contexto</h1>
-      <p className="header-description">
+    <header className={styles.header}>
+      <h1 className={styles.header_title}>Contexto</h1>
+      <p className={styles.header_description}>
         Guess the word based on the clues provided by your friends!
       </p>
     </header>
