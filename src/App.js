d45a14c49bb932291bc6e808531d446f36d35bc6
Katiasun <katjatolok3@gmail.com>
2024-05-09 15:18:33 +0200

    create folder DropDownMenu. Also write code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header.jsx";
 import GameBoard from "./components/gameBoard/GameBoard.jsx";
+import DropDownMenu from "./components/dropDownMenu/DropDownMenu.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DropDownMenu />
       <GameBoard />
     </div>
   );
