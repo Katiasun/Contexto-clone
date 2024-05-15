db0d1d961f57f73ff892cd9d67e0a3a910be00ac
Katiasun <katjatolok3@gmail.com>
2024-05-15 21:11:38 +0200

    State to track if the modal is open or closed

--- src/components/modalWindow/ModalWindow.jsx
+++ src/components/modalWindow/ModalWindow.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ModalWindow() {
+  const [isOpen, setIsOpen] = useState(false);
   return <button>knopka</button>;
 }
