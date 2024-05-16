350f82b5059fc676d85daf6ee66bebb2d2a7652b
Katiasun <katjatolok3@gmail.com>
2024-05-16 23:41:41 +0200

    write overflow for body tag

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.headerTitle}>Contexto</h1>
-      <p className={styles.headerDescription}>
-        Guess the word based on the clues provided by your friends!
-      </p>
+      <div className={styles.game}></div>
     </header>
   );
 }
