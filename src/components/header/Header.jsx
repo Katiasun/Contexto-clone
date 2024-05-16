647ae0fe51646c111c91124475b01fafa9f32847
Katiasun <katjatolok3@gmail.com>
2024-05-16 23:51:14 +0200

    rewrite code in component Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,14 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.headerTitle}>Contexto</h1>
-      <div className={styles.game}></div>
+      <div className={styles.numbersBlock}>
+        <p>
+          Game: <span>#666</span>
+        </p>
+        <p>
+          Guesses: <span></span>
+        </p>
+      </div>
     </header>
   );
 }
